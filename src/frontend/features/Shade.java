abb51edb90d2849aa6d0eff6c06d9e11c682ccba
Lucia Oliveto <luciamoliveto@gmail.com>
2024-06-30 13:57:39 -0300

    refactor toString

--- src/frontend/features/Shade.java
+++ src/frontend/features/Shade.java
@@ -5,48 +5,24 @@ import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.paint.Color;
 
 public enum Shade {
-
-    NOSHADE(0, true) {
-        @Override
-        public String toString() {
-            return "Sin sombra";
-        }
+    NOSHADE(0, true, "Sin sombra") {
         @Override
         public void drawShade(GraphicsContext gc, Drawable figure, Color color) {}
     },
-    SIMPLE(+10, true) {
-        @Override
-        public String toString() {
-            return "Simple";
-        }
-    },
-    SIMPLEINVERTED(-10, true){
-        @Override
-        public String toString() {
-            return "Simple inversa";
-        }
-    },
-    COLORED(10, false) {
-        @Override
-        public String toString() {
-            return "Coloreada";
-        }
-    },
-    COLOREDINVERTED(-10, false) {
-        @Override
-        public String toString() {
-            return "Coloreada inversa";
-        }
-    };
-
-    Shade(int offset, boolean useDefaultColor) {
-        this.offset = offset;
-        this.useDefaultColor = useDefaultColor;
-    }
-
+    SIMPLE(10, true, "Simple"),
+    SIMPLEINVERTED(-10, true, "Simple inversa"),
+    COLORED(10, false, "Coloreada"),
+    COLOREDINVERTED(-10, false, "Coloreada inversa") ;
     private static final Color DEFAULT_COLOR = Color.GRAY;
     private final int offset;
     private final boolean useDefaultColor;
+    private final String name;
+
+    Shade(int offset, boolean useDefaultColor, String name) {
+        this.offset = offset;
+        this.useDefaultColor = useDefaultColor;
+        this.name = name;
+    }
 
     public void drawShade(GraphicsContext gc, Drawable figure, Color figureColor) {
         gc.setFill( this.useDefaultColor ? DEFAULT_COLOR : figureColor.darker());
@@ -54,5 +30,8 @@ public enum Shade {
         figure.draw(gc);
         figure.move(-this.offset, -this.offset);
     }
-
+    @Override
+    public String toString() {
+        return name;
+    }
 }
